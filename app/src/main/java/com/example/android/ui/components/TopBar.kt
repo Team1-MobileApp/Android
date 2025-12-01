18b1f315f720e16806fa46112003d8ee31028e55
leeseungeon <dbleecomkr@naver.com>
2025-12-01 21:32:44 +0900

    Feat: #25 logo image add

--- app/src/main/java/com/example/android/ui/components/TopBar.kt
+++ app/src/main/java/com/example/android/ui/components/TopBar.kt
@@ -3,24 +3,38 @@ package com.example.android.ui.components
 import android.content.Intent
 import android.provider.MediaStore
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
-    title: String,
+    imageRes: Int? = null,
     onCameraClick: () -> Unit,
     showBackButton: Boolean = false,
     onBackClick: (() -> Unit)? = null
 ) {
     CenterAlignedTopAppBar(
-        title = { Text(text = title) },
+        title = {
+            if (imageRes != null) {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "Top Bar Image",
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+        },
         navigationIcon = {
             if (showBackButton && onBackClick != null) {
                 IconButton(onClick = onBackClick) {
